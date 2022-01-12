449913cfb457f83c76897164754f56cfb5dd0cb2
cxz <2405819294@qq.com>
2022-01-12 22:52:45 +0800

    引入全局组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
 Money.vue
+<Nav></Nav>
     </div>
 </template>
 
 <script>
+import Nav from '@/components/Nav.vue';
     export default {
-        
+        name:'Money',
+        components:{Nav}
     }
 </script>
 
