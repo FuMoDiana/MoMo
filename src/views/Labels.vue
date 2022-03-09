71207c329c48d707b8af0a7fd54bf984d6b5fbcb
cxz <2405819294@qq.com>
2022-03-09 16:44:28 +0800

    增加了标签界面和标签编辑界面的交互

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
         <Layout>
-            <ol class="tags">
-      <li v-for="tag in tags" :key="tag.id">
+          <div class="tags">
+      <router-link class="tag"
+                   v-for="tag in tags" :key="tag.id"
+                   :to="`/labels/edit/${tag.id}`">
         <span>{{tag.name}}</span>
         <Icon name="right"/>
-      </li>
-    </ol>
+      </router-link>
+    </div>
     <div class="createTag-wrapper">
       <button class="createTag" @click="createTag">新建标签</button>
     </div>
@@ -41,7 +43,7 @@
 <style lang="scss" scoped>
  .tags{
      font-size: 16px;
-     >li{
+     >.tag{
         min-height: 44px;
         display:flex;
         align-items: center;
